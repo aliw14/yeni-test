0e929f8cc3529029fb1e835cb844b17662fcff49
ali <ali@MacBook-Pro-Ali.local>
2022-12-22 19:51:46 +0400

    salam

--- yeni test/Program.cs
+++ yeni test/Program.cs
@@ -3,7 +3,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("hello world");
+        Console.WriteLine("test");
     }
 }
 
